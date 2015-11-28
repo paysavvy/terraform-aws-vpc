7f1d8cccc6d20ec19ab481e8ab42cfa360b808c1
serj <sv.krasnov@gmail.com>
2015-11-28 13:07:07 -0800

    adjust config for us

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -1,15 +1,15 @@
 provider "aws" {
 	access_key = "${var.aws_access_key}"
 	secret_key = "${var.aws_secret_key}"
-	region = "us-east-1"
+	region = "us-west-2"
 }
 
-resource "aws_vpc" "default" {
+resource "aws_vpc" "gastown-test" {
 	cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_internet_gateway" "default" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_internet_gateway" "gastown-test" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 }
 
 # NAT instance
@@ -22,25 +22,25 @@ resource "aws_security_group" "nat" {
 		from_port = 0
 		to_port = 65535
 		protocol = "tcp"
-		cidr_blocks = ["${aws_subnet.us-east-1b-private.cidr_block}"]
+		cidr_blocks = ["${aws_subnet.us-west-2b-private.cidr_block}"]
 	}
 	ingress {
 		from_port = 0
 		to_port = 65535
 		protocol = "tcp"
-		cidr_blocks = ["${aws_subnet.us-east-1d-private.cidr_block}"]
+		cidr_blocks = ["${aws_subnet.us-west-2c-private.cidr_block}"]
 	}
 
-	vpc_id = "${aws_vpc.default.id}"
+	vpc_id = "${aws_vpc.gastown-test.id}"
 }
 
 resource "aws_instance" "nat" {
 	ami = "${var.aws_nat_ami}"
-	availability_zone = "us-east-1b"
+	availability_zone = "us-west-2b"
 	instance_type = "m1.small"
 	key_name = "${var.aws_key_name}"
 	security_groups = ["${aws_security_group.nat.id}"]
-	subnet_id = "${aws_subnet.us-east-1b-public.id}"
+	subnet_id = "${aws_subnet.us-west-2b-public.id}"
 	associate_public_ip_address = true
 	source_dest_check = false
 }
@@ -52,61 +52,61 @@ resource "aws_eip" "nat" {
 
 # Public subnets
 
-resource "aws_subnet" "us-east-1b-public" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_subnet" "us-west-2b-public" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	cidr_block = "10.0.0.0/24"
-	availability_zone = "us-east-1b"
+	availability_zone = "us-west-2b"
 }
 
-resource "aws_subnet" "us-east-1d-public" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_subnet" "us-west-2c-public" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	cidr_block = "10.0.2.0/24"
-	availability_zone = "us-east-1d"
+	availability_zone = "us-west-2c"
 }
 
 # Routing table for public subnets
 
-resource "aws_route_table" "us-east-1-public" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_route_table" "us-west-2-public" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	route {
 		cidr_block = "0.0.0.0/0"
-		gateway_id = "${aws_internet_gateway.default.id}"
+		gateway_id = "${aws_internet_gateway.gastown-test.id}"
 	}
 }
 
-resource "aws_route_table_association" "us-east-1b-public" {
-	subnet_id = "${aws_subnet.us-east-1b-public.id}"
-	route_table_id = "${aws_route_table.us-east-1-public.id}"
+resource "aws_route_table_association" "us-west-2b-public" {
+	subnet_id = "${aws_subnet.us-west-2b-public.id}"
+	route_table_id = "${aws_route_table.us-west-2-public.id}"
 }
 
-resource "aws_route_table_association" "us-east-1d-public" {
-	subnet_id = "${aws_subnet.us-east-1d-public.id}"
-	route_table_id = "${aws_route_table.us-east-1-public.id}"
+resource "aws_route_table_association" "us-west-2c-public" {
+	subnet_id = "${aws_subnet.us-west-2c-public.id}"
+	route_table_id = "${aws_route_table.us-west-2-public.id}"
 }
 
 # Private subsets
 
-resource "aws_subnet" "us-east-1b-private" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_subnet" "us-west-2b-private" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	cidr_block = "10.0.1.0/24"
-	availability_zone = "us-east-1b"
+	availability_zone = "us-west-2b"
 }
 
-resource "aws_subnet" "us-east-1d-private" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_subnet" "us-west-2c-private" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	cidr_block = "10.0.3.0/24"
-	availability_zone = "us-east-1d"
+	availability_zone = "us-west-2c"
 }
 
 # Routing table for private subnets
 
-resource "aws_route_table" "us-east-1-private" {
-	vpc_id = "${aws_vpc.default.id}"
+resource "aws_route_table" "us-west-2-private" {
+	vpc_id = "${aws_vpc.gastown-test.id}"
 
 	route {
 		cidr_block = "0.0.0.0/0"
@@ -114,14 +114,14 @@ resource "aws_route_table" "us-east-1-private" {
 	}
 }
 
-resource "aws_route_table_association" "us-east-1b-private" {
-	subnet_id = "${aws_subnet.us-east-1b-private.id}"
-	route_table_id = "${aws_route_table.us-east-1-private.id}"
+resource "aws_route_table_association" "us-west-2b-private" {
+	subnet_id = "${aws_subnet.us-west-2b-private.id}"
+	route_table_id = "${aws_route_table.us-west-2-private.id}"
 }
 
-resource "aws_route_table_association" "us-east-1d-private" {
-	subnet_id = "${aws_subnet.us-east-1d-private.id}"
-	route_table_id = "${aws_route_table.us-east-1-private.id}"
+resource "aws_route_table_association" "us-west-2c-private" {
+	subnet_id = "${aws_subnet.us-west-2c-private.id}"
+	route_table_id = "${aws_route_table.us-west-2-private.id}"
 }
 
 # Bastion
@@ -137,16 +137,16 @@ resource "aws_security_group" "bastion" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
-	vpc_id = "${aws_vpc.default.id}"
+	vpc_id = "${aws_vpc.gastown-test.id}"
 }
 
 resource "aws_instance" "bastion" {
 	ami = "${var.aws_ubuntu_ami}"
-	availability_zone = "us-east-1b"
+	availability_zone = "us-west-2b"
 	instance_type = "t2.micro"
 	key_name = "${var.aws_key_name}"
 	security_groups = ["${aws_security_group.bastion.id}"]
-	subnet_id = "${aws_subnet.us-east-1b-public.id}"
+	subnet_id = "${aws_subnet.us-west-2b-public.id}"
 }
 
 resource "aws_eip" "bastion" {
--- variables.tf
+++ variables.tf
@@ -3,8 +3,8 @@ variable "aws_secret_key" {}
 variable "aws_key_path" {}
 variable "aws_key_name" {}
 variable "aws_nat_ami" {
-	default = "ami-2e1bc047"
+	default = "ami-030f4133"
 }
 variable "aws_ubuntu_ami" {
-	default = "ami-b227efda"
+	default = "ami-d5c5d1e5"
 }
