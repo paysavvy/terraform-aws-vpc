dfa68ccc18a43523351354d8b7c14c232437d323
Mike Martinson <mike.j.martinson@gmail.com>
2015-11-28 14:15:22 -0800

    Add egress rule to bastion to allow ssh to private

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -173,6 +173,13 @@ resource "aws_security_group" "bastion" {
 		cidr_blocks = ["0.0.0.0/0"]
 	}
 
+	egress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
 	vpc_id = "${aws_vpc.gastown-test.id}"
 }
 
